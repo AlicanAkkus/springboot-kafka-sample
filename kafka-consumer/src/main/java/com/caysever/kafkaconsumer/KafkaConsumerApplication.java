e2d6809095a6baaeb6d0a44f7773f95107609c18
Alican Akkus <alican.akkus94@gmail.com>
2018-10-13 18:08:56 +0300

    we can send json message and consume it now.

--- kafka-consumer/src/main/java/com/caysever/kafkaconsumer/KafkaConsumerApplication.java
+++ kafka-consumer/src/main/java/com/caysever/kafkaconsumer/KafkaConsumerApplication.java
@@ -1,10 +1,27 @@
 package com.caysever.kafkaconsumer;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Builder;
+import lombok.Data;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class KafkaConsumerApplication {
@@ -14,11 +31,53 @@ public class KafkaConsumerApplication {
     }
 }
 
+@Data
+@Builder
+class Trip {
+
+    private Long id;
+    private Long userId;
+    private Long driverId;
+
+}
+
 @Service
 class KafkaMessageConsumer {
 
-    @KafkaListener(topics = "trip-TRIP_STARTED")
-    public void consume(ConsumerRecord<String, String> record) {
-        System.out.println(record.value());
+    @KafkaListener(topics = "test")
+    public void consume(@Payload Trip trip, @Headers MessageHeaders messageHeaders) {
+        System.out.println(messageHeaders);
+        System.out.println(trip);
+    }
+}
+
+@Configuration
+class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
+
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, Trip> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs(), new StringDeserializer(), new JsonDeserializer<>(Trip.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Trip> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Trip> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+
+        return factory;
     }
 }
--- kafka-producer/src/main/java/com/caysever/kafkaproducer/KafkaProducerApplication.java
+++ kafka-producer/src/main/java/com/caysever/kafkaproducer/KafkaProducerApplication.java
@@ -1,10 +1,27 @@
 package com.caysever.kafkaproducer;
 
+import lombok.Builder;
+import lombok.Data;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class KafkaProducerApplication {
@@ -14,7 +31,48 @@ public class KafkaProducerApplication {
     }
 
     @Bean
-    public CommandLineRunner initalize(KafkaTemplate<String, String> kafkaTemplate) {
-        return args -> kafkaTemplate.send("trip-TRIP_STARTED", "test data");
+    public CommandLineRunner initialize(KafkaTemplate<String, Trip> kafkaTemplate) {
+        return args -> {
+            Trip trip = Trip.builder().id(1L).driverId(100L).userId(200L).build();
+            kafkaTemplate.send("test", trip);
+        };
+    }
+}
+
+@Data
+@Builder
+class Trip {
+
+    private Long id;
+    private Long userId;
+    private Long driverId;
+
+}
+
+@Configuration
+class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaServers;
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, Trip> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Trip> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
